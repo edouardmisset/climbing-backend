0cb00be26d7aa9412f60a2d3a8c28782ee870960
Edouard Misset <edouardmisset@gmail.com>
2024-05-04 11:54:09 +0200

    feat: add frequency endpoints for areas and crags

--- src/routes/areas.ts
+++ src/routes/areas.ts
@@ -3,8 +3,9 @@ import { Hono } from 'hono'
 import ascentJSON from '@data/ascent-data.json' with { type: 'json' }
 import { ascentSchema } from '@schema/ascent.ts'
 import { etag } from 'hono/middleware'
-import { removeAccents } from '@edouardmisset/utils'
+import { frequency } from '@edouardmisset/utils'
 import { findSimilar } from '@helpers/find-similar.ts'
+import { sortNumericalValues } from '@helpers/sort-values.ts'
 
 const parsedAscents = ascentSchema.array().parse(ascentJSON.data)
 
@@ -16,10 +17,20 @@ app.get('/', (ctx) => {
   const areas = Array.from(
     new Set(parsedAscents.map(({ area }) => area?.trim())),
   )
-    .filter((crag) => crag != null)
+    .filter((crag) => crag !== undefined)
     .sort()
 
   return ctx.json({ data: areas })
+})
+
+// Get areas frequency
+app.get('/frequency', (ctx) => {
+  const areas = parsedAscents.map(({ area }) => area).filter((a) =>
+    a !== undefined
+  ) as string[]
+
+  const sortedAreasByFrequency = sortNumericalValues(frequency(areas), false)
+  return ctx.json({ data: sortedAreasByFrequency })
 })
 
 // Deduplicate the areas
--- src/routes/crags.ts
+++ src/routes/crags.ts
@@ -3,8 +3,9 @@ import { Hono } from 'hono'
 import ascentJSON from '@data/ascent-data.json' with { type: 'json' }
 import { ascentSchema } from '@schema/ascent.ts'
 import { etag } from 'hono/middleware'
-import { removeAccents } from '@edouardmisset/utils'
+import { frequency } from '@edouardmisset/utils'
 import { findSimilar } from '@helpers/find-similar.ts'
+import { sortNumericalValues } from '@helpers/sort-values.ts'
 
 const parsedAscents = ascentSchema.array().parse(ascentJSON.data)
 
@@ -16,10 +17,17 @@ app.get('/', (ctx) => {
   const crags = Array.from(
     new Set(parsedAscents.map(({ crag }) => crag.trim())),
   )
-    .filter((crag) => crag != null)
     .sort()
 
   return ctx.json({ data: crags })
+})
+
+// Get crags frequency
+app.get('/frequency', (ctx) => {
+  const crags = parsedAscents.map(({ crag }) => crag)
+
+  const sortedCragsByFrequency = sortNumericalValues(frequency(crags), false)
+  return ctx.json({ data: sortedCragsByFrequency })
 })
 
 app.get('/duplicates', (ctx) => {
