7b7d715b0db42787bda0f5343a1f7f1c2f625154
Edouard Misset <edouardmisset@gmail.com>
2024-05-09 11:32:10 +0200

    test cron 2

--- src/prod.ts
+++ src/prod.ts
@@ -6,3 +6,7 @@ Deno.env.set('PORT', '8000')
 Deno.serve({
   port: Number(Deno.env.get('PORT')) || FALLBACK_PORT,
 }, (req) => api.fetch(req, { ENV: Deno.env.get('ENV') }))
+
+Deno.run({
+  cmd: ['deno', 'run', '--allow-net', '--allow-read', '--allow-write', '--unstable-cron', 'src/scripts/cron.ts'],
+})
