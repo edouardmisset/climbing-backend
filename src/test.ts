30f568fccb4e0e3fd5997e519d63c1b02c28f7dc
Edouard Misset <edouardmisset@gmail.com>
2024-09-14 01:19:54 +0200

    chore: improve dev experience (start up)

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,19 @@
+import { assert } from 'jsr:@std/assert@^0.223.0/assert'
+import { TEST_PORT } from '../benchmark/test-data.ts'
 import api, { FALLBACK_PORT } from './api.ts'
 
 Deno.env.set('ENV', 'test')
-Deno.env.set('PORT', '8001')
+Deno.env.set('PORT', String(TEST_PORT))
+
+const port = Number(Deno.env.get('PORT')) || FALLBACK_PORT + 1
 
 Deno.serve({
-  port: Number(Deno.env.get('PORT')) || FALLBACK_PORT + 1,
+  port,
 }, (req) => api.fetch(req, { ENV: Deno.env.get('ENV') }))
+
+console.log('TEST')
+
+Deno.test('GET /api is ok', async () => {
+  const res = await api.request('/api')
+  assert(res.status === 200)
+})
