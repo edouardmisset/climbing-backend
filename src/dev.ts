c1c16b2a7e0552f99cfea09d9c11a61f7136b2be
Edouard Misset <edouardmisset@gmail.com>
2024-09-28 22:48:46 +0200

    chore: show all routes when starting the dev server

--- src/dev.ts
+++ src/dev.ts
@@ -1,4 +1,5 @@
 import api, { FALLBACK_PORT } from './api.ts'
+import { showRoutes } from 'hono/dev'
 
 Deno.env.set('ENV', 'dev')
 Deno.env.set('PORT', '8000')
@@ -6,3 +7,5 @@ Deno.env.set('PORT', '8000')
 Deno.serve({
   port: Number(Deno.env.get('PORT')) || FALLBACK_PORT,
 }, (req) => api.fetch(req, { ENV: Deno.env.get('ENV') }))
+
+showRoutes(api)
