a6ce10d2fa889e45bc6de844317ad622ec1175d2
Edouard Misset <edouardmisset@gmail.com>
2024-11-17 13:51:16 +0100

    fix: env variable

--- src/mod.ts
+++ src/mod.ts
@@ -1,15 +1,18 @@
+import { load } from '@std/dotenv'
 import { blue, bold, green } from '@std/fmt/colors'
 import { inspectRoutes } from 'hono/dev'
 import { getAllAscents } from 'services/ascents.ts'
 import { getAllTrainingSessions } from 'services/training.ts'
 import { app } from './app.ts'
 
+await load({ export: true })
+export const env = Deno.env.toObject()
 const FALLBACK_PORT = 8000
 
-const port = Number(Deno.env.get('PORT')) || FALLBACK_PORT
+const port = Number(env.PORT) || FALLBACK_PORT
 Deno.serve({ port }, app.fetch)
 
-if (Deno.env.get('ENV') === 'dev') {
+if (env.ENV === 'dev') {
   // Display the "GET" routes in the console for easy access during development.
   displayRoutes()
 
