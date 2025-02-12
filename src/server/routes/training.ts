45f2c51826411d27209c1ba8799ddd6ccc5f3be7
Edouard Misset <edouardmisset@gmail.com>
2025-02-12 22:18:25 +0100

    feat: add filtering for training sessions by year

--- src/server/routes/training.ts
+++ src/server/routes/training.ts
@@ -4,5 +4,11 @@ import { getAllTrainingSessions } from 'services/training.ts'
 
 export const training = new Hono().get(
   '/',
-  async (c) => c.json({ data: await getAllTrainingSessions() }),
+  async (c) => {
+    const year = c.req.query('year')
+    const sessions = await getAllTrainingSessions()
+
+    const filteredSessions = sessions.filter((session) => year === undefined || year === '' ? true : new Date(session.date).getFullYear().toString() === year)
+    return c.json({ data: filteredSessions })
+  },
 )
